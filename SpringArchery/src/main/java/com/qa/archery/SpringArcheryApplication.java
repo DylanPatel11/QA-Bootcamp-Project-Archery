cedbdae7d4728f85651faaa3b1d7b8338ba114ed
DylanPatel11 <me@dylanpatel.co.uk>
2022-03-21 02:20:26 +0000

    DP: Java file added

---
+++ SpringArchery/src/main/java/com/qa/archery/SpringArcheryApplication.java
@@ -0,0 +1,13 @@
+package com.qa.archery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArcheryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArcheryApplication.class, args);
+	}
+
+}
